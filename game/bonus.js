80a7889ed493e1cb8ce09fa1d35e5205a70a9fc3
chenshengliang <chenshengliang@corp.netease.com>
2023-05-12 22:40:12 +0800

    [fun]console

--- game/bonus.js
+++ game/bonus.js
@@ -2,7 +2,10 @@ var a = 0
 
 module.exports = {
     newTask(req) {
-        console.log(JSON.stringify(req.body().data))
+        console.log(JSON.stringify(req.body))
+        console.log(JSON.stringify(req.params))
+        console.log(JSON.stringify(req.query))
+        console.log(JSON.stringify(req.headers))
         return ++a
     }
 }
